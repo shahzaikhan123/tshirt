38d1b7ee37a5f3ab37dcd2dc320caa07108dc233
shahzaikhan123 <52047135+shahzaikhan123@users.noreply.github.com>
2020-03-30 15:19:52 +0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,32 @@
+    
+    
+    
+<div class="navbar navbar-inverse navbar-fixed-top" >
+        <div class="container-fluid  " >    
+            
+            <a href="#" class="navbar-brand">sk Store</a>
+        
+          
+            <ul class="nav navbar-nav  ">
+                <li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+                <li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
+            </ul>
+            
+            <form class="navbar-form navbar-left">
+               <div class="form-group">  
+                  <input type="text" placeholder="Search" id="search">   
+           
+                  <button type="submit" class="btn btn-primary" id="search_btn"><span class="glyphicon glyphicon-search"></span></button>
+              </div>
+          </form>
+
+        <ul class="nav navbar-nav navbar-right">
+             <li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span>Cart<span class="badge">0</span></a>
+                        
+            </li>
+                    <li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span>SignIn</a></li>
+        </ul>
+        </div>              
+    </div> 
+    
---
+++ index.php
@@ -0,0 +1,193 @@
+
+
+<?php include 'css.css';?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>HS Store</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+  		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	</head>
+<body>
+
+	
+<div class="container" id="aa" >
+	<div class="row">
+		<div class="col-md-3 col-sm-6">
+			<div class="product-grid">
+				<div class="product-image">
+				<a href="">	
+					<img  class="pic-1" src="1.jpg" alt="s">
+				</a>
+	
+  						<ul class="social">
+  							<li><a href="#" data-tip="Quick View"> <i class="fa fa-search"></i></a></li>
+  							
+                            <li><a href="" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+  						</ul>
+  							
+  					<div class="product-content">
+  						<h3 class="title"><a href="#">Woman's Blouse</a></h3>
+  						<div class="price">$16.00<span>$20.00</span></div>
+  						<a class="add-to-cart" href="#">+ Add To Cart</a>
+  					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6">
+			<div class="product-grid">
+				<div class="product-image">
+				<a href="">	
+					<img class="pic-1" src="1.jpg" alt="s">
+				</a>
+	
+  						<ul class="social">
+  							<li><a href="#" data-tip="Quick View"> <i class="fa fa-search"></i></a></li>
+  							
+                            <li><a href="" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+  						</ul>
+  							
+  					<div class="product-content">
+  						<h3 class="title"><a href="#">Woman's Blouse</a></h3>
+  						<div class="price">$16.00<span>$20.00</span></div>
+  						<a class="add-to-cart" href="#">+ Add To Cart</a>
+  					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6">
+			<div class="product-grid">
+				<div class="product-image">
+				<a href="">	
+					<img class="pic-1" src="1.jpg" alt="s">
+				</a>
+	
+  						<ul class="social">
+  							<li><a href="#" data-tip="Quick View"> <i class="fa fa-search"></i></a></li>
+  							
+                            <li><a href="" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+  						</ul>
+  							
+  					<div class="product-content">
+  						<h3 class="title"><a href="#">Woman's Blouse</a></h3>
+  						<div class="price">$16.00<span>$20.00</span></div>
+  						<a class="add-to-cart" href="#">+ Add To Cart</a>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  		<div class="col-md-3 col-sm-6">
+			<div class="product-grid">
+				<div class="product-image">
+				<a href="">	
+					<img class="pic-1" src="1.jpg" alt="s">
+				</a>
+	
+  						<ul class="social">
+  							<li><a href="#" data-tip="Quick View"> <i class="fa fa-search"></i></a></li>
+  							
+                            <li><a href="" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+  						</ul>
+  							
+  					<div class="product-content">
+  						<h3 class="title"><a href="#">Woman's Blouse</a></h3>
+  						<div class="price">$16.00<span>$20.00</span></div>
+  						<a class="add-to-cart" href="#">+ Add To Cart</a>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  	</div>
+  	<div class="row">
+  		<div class="col-md-3 col-sm-6">
+			<div class="product-grid">
+				<div class="product-image">
+				<a href="">	
+					<img class="pic-1"  src="1.jpg" alt="s">
+				</a>
+	
+  						<ul class="social">
+  							<li><a href="#" data-tip="Quick View"> <i class="fa fa-search"></i></a></li>
+  							
+                            <li><a href="" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+  						</ul>
+  							
+  					<div class="product-content">
+  						<h3 class="title"><a href="#">Woman's Blouse</a></h3>
+  						<div class="price">$16.00<span>$20.00</span></div>
+  						<a class="add-to-cart" href="#">+ Add To Cart</a>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  	</div>
+<div class="page_nav">
+							<a href=""><i class="fa fa-angle-left"></i></a>
+							<a href="" class="active">1</a>
+							<a href="">2</a>
+							<a href="">3</a>
+							<a class="no-active">...</a>
+							<a href="">9</a>
+							<a href=""><i class="fa fa-angle-right"></i></a>
+						</div>
+</div>
+
+</body>
+<?php include 'footer.php';?>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
